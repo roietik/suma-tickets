45b404cb3009da9a50a128e68ce59b51c3a89d87
Radek <radoslaw.grzymala@hotmail.com>
2024-07-11 11:56:35 +0200

    feat: refactoring checking errors for emails

--- api/controllers/emails.controller.js
+++ api/controllers/emails.controller.js
@@ -3,10 +3,10 @@ const {sendEmailGmail} = require("../services/send-email.service");
 async function sendEmail(request, response) {
     try {
         await sendEmailGmail(request, response);
-        response.status(200).send('email was send');
+        response.status(200).send({message: 'Bilet został wysłany na skrzynkę email'});
     } catch (error) {
         console.error('Error sending email:', error);
-        response.status(500).send('Email sending failed');
+        response.status(500).send('Email sending failed', error);
     }
 }
 
